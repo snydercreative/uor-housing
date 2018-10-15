350348c289024408e37196982a2c1710cca901bf
snydercreative <me@michaelsnyder.me>
2018-10-15 16:14:32 -0400

    Initial buildout

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "semi": "off",
+        "no-tabs": "off",
+        "indent": "off"
+    },
+    "globals": {
+        "fetch": true
+    }
+};
---
+++ index.js
@@ -0,0 +1,34 @@
+import aws from 'aws-sdk'
+import diff from 'diff'
+
+let differences = ''
+let lastUploadContent = ''
+
+const handleBucketObject = (err, freshData) => {
+	const options = {
+		newlineIsToken: true,
+	}
+
+	differences = diff.diffTrimmedLines(lastUploadContent, freshData, options)
+}
+
+exports.handler = async (event, context) => {
+	const s3options = {
+		accessKeyId: process.env.AWS_KEY,
+		secretAccessKey: process.env.AWS_SECRET,
+	}
+
+	const params = {
+		Bucket: event.Records[0].s3.bucket.name,
+		Key: event.Records[0].s3.object.key,
+	}
+
+	const lastUpload = await fetch('http://www.uorenaissance.com/map/house.txt')
+	lastUploadContent = await lastUpload.text()
+
+	const s3 = new aws.S3(s3options)
+
+	s3.getObject(params, handleBucketObject)
+
+	return differences
+}
