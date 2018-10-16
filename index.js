63286bd1fb3a1f2c125dbda864f0a1b960392987
snydercreative <me@michaelsnyder.me>
2018-10-15 21:52:16 -0400

    Fixed

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const aws = require('aws-sdk')
-const diff = require('diff')
+const AWS = require('aws-sdk')
 const fetch = require('node-fetch')
+const _ = require('lodash')
 
 let differences = ''
-let lastUploadContent = ''
+let freshData = ''
 
-const handleBucketObject = (err, freshData) => {
-	const options = {
-		newlineIsToken: true,
-	}
+const handleBucketObject = (err, lastUpload, callback) => {
+	const freshHouses = freshData.split(/\r\n/).sort()
+	const lastUploadHouses = lastUpload.Body.toString().split(/\r\n/).sort()
 
-	differences = diff.diffTrimmedLines(lastUploadContent, freshData, options)
+	differences = _.difference(lastUploadHouses, freshHouses)
+
+	callback(null, differences)
 }
 
-exports.handler = async (event) => {
+exports.handler = (event, context, callback) => {
 	const s3options = {
 		accessKeyId: process.env.AWS_KEY,
 		secretAccessKey: process.env.AWS_SECRET,
+		apiVersion: '2006-03-01',
+		region: 'us-west-2',
 	}
+
+	const s3 = new AWS.S3(s3options)
 
 	const params = {
-		Bucket: event.Records[0].s3.bucket.name,
-		Key: event.Records[0].s3.object.key,
+		Bucket: 'uor-housing',
+		Key: 'house.txt',
 	}
 
-	const lastUpload = await fetch('http://www.uorenaissance.com/map/house.txt')
-	lastUploadContent = await lastUpload.text()
+	fetch('http://www.uorenaissance.com/map/house.txt')
+		.then(freshDataResponse => freshDataResponse.text())
+		.then((freshDataBody) => {
+			freshData = freshDataBody
 
-	const s3 = new aws.S3(s3options)
-
-	s3.getObject(params, handleBucketObject)
-
-	return differences
+			s3.getObject(params, (err, body) => handleBucketObject(err, body, callback))
+		})
 }
